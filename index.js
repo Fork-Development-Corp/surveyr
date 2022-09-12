293941b1c03b111c7de3463432617119cc532882
J. Campbell <fork42@mac.com>
2022-09-11 21:30:13 -0500

    adds index.js with initial exported func

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.printMsg = function () {
+    console.log("This is a message from the Surveyr package.");
+};
+exports.surveyrMsg = function () {
+    console.log("This is a message from the Surveyr package.");
+};
