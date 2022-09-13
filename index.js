1f3bda442e67ad3f10dd6e16a0264543acf16999
J. Campbell <fork42@mac.com>
2022-09-12 19:07:22 -0500

    update basic exported functions

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 exports.printMsg = function () {
-    console.log("This is a message from the Surveyr package.");
-};
-exports.surveyrMsg = function () {
-    console.log("This is a message from the Surveyr package.");
+    console.log("This is a message from the preventscripts-library package.");
 };
